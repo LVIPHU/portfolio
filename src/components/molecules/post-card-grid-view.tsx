fe031110740c2ceac5a42dd75db8140a784dda0d
Lương Vĩ Phú <kenlock.lvp@gmail.com>
2025-04-20 20:06:27 +0700

    [update] UX UI blog

--- src/components/molecules/post-card-grid-view.tsx
+++ src/components/molecules/post-card-grid-view.tsx
@@ -1,7 +1,7 @@
 import type { Blog } from '@contentlayer/generated'
 import type { CoreContent } from '@/types/data'
 import { formatDate } from '@/utils'
-import { Card, CardContent, CardHeader, GrowingUnderline, Image, NavigationLink } from '@/components/atoms'
+import { Card, CardContent, CardFooter, CardHeader, GrowingUnderline, Image, NavigationLink } from '@/components/atoms'
 import { SITE_METADATA } from '@data/site-metadata'
 import { Calendar, ClockIcon } from 'lucide-react'
 import { TagsList } from '@/components/molecules/tags'
@@ -9,8 +9,8 @@ import { TagsList } from '@/components/molecules/tags'
 export function PostCardGridView({ post }: { post: CoreContent<Blog> }) {
   const { path, date, title, tags, images, readingTime } = post
   return (
-    <article>
-      <Card className='shadow-none'>
+    <article className={'h-full'}>
+      <Card className='shadow-none h-full flex flex-col'>
         <CardHeader className='p-2'>
           <NavigationLink href={`/${path}`}>
             <Image
@@ -22,26 +22,26 @@ export function PostCardGridView({ post }: { post: CoreContent<Blog> }) {
             />
           </NavigationLink>
         </CardHeader>
-        <CardContent className='pb-5 pt-4'>
+        <CardContent className='py-5'>
           <TagsList tags={tags} />
           <h3 className='mt-4 text-[1.35rem] font-semibold tracking-tight'>
             <NavigationLink href={`/${path}`}>
               <GrowingUnderline>{title}</GrowingUnderline>
             </NavigationLink>
           </h3>
-          <div className='mt-6 flex items-center justify-between'>
-            <div className='flex items-center gap-2'>
-              <Calendar className='size-4' />
-              <time dateTime={date} className='text-sm text-muted-foreground'>
-                {formatDate(date)}
-              </time>
-            </div>
-            <div className='flex items-center gap-2'>
-              <ClockIcon className='size-4' />
-              <span className='text-sm text-muted-foreground'>{Math.ceil(readingTime.minutes)} mins read</span>
-            </div>
-          </div>
         </CardContent>
+        <CardFooter className='mt-auto flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <Calendar className='size-4' />
+            <time dateTime={date} className='text-sm text-muted-foreground'>
+              {formatDate(date)}
+            </time>
+          </div>
+          <div className='flex items-center gap-2'>
+            <ClockIcon className='size-4' />
+            <span className='text-sm text-muted-foreground'>{Math.ceil(readingTime.minutes)} mins read</span>
+          </div>
+        </CardFooter>
       </Card>
     </article>
   )
